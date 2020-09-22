05796fa364144c5c842f364afcf3e53238301f6c
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-22 16:03:04 +0530

    [swapnaneel]UC3:Player checking for option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,5 +4,20 @@ public class SnakeAndLadder
 {
     int start_pos=0;
     int die_number=(int)(Math.random()*6+1);
+    int option= (int)(Math.random()*3+1);
+    switch(option)
+    {
+        case 1:
+            start_pos+=0;
+            break;
+        case 2:
+            start_pos+=die_number;
+            break;
+        case 3:
+            start_pos-=die_number;
+            break;
+        default:
+            start_pos=start_pos;
+    }
 }
 }
