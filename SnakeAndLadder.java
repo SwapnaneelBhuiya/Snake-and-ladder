3c57bdc2ad44ab7b26a4065d5e44ea35973ac90c
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-22 16:06:04 +0530

    [swapnaneel]UC4: Repeat till player reaches 100 ensuring start position not less than 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,21 +3,24 @@ public class SnakeAndLadder
     public static void main(String args[])
 {
     int start_pos=0;
-    int die_number=(int)(Math.random()*6+1);
-    int option= (int)(Math.random()*3+1);
-    switch(option)
-    {
-        case 1:
-            start_pos+=0;
-            break;
-        case 2:
-            start_pos+=die_number;
-            break;
-        case 3:
-            start_pos-=die_number;
-            break;
-        default:
-            start_pos=start_pos;
+    while(start_pos<=100) {
+        int die_number = (int) (Math.random() * 6 + 1);
+        int option = (int) (Math.random() * 3 + 1);
+        switch (option) {
+            case 1:
+                start_pos += 0;
+                break;
+            case 2:
+                start_pos += die_number;
+                break;
+            case 3:
+                start_pos -= die_number;
+                break;
+            default:
+                start_pos = start_pos;
+        }
+        if(start_pos<0)
+            start_pos=0;
     }
 }
 }
