59a7470127567decddaab0740c51cda8fd0133b7
SwapnaneelBhuiya <43994683+SwapnaneelBhuiya@users.noreply.github.com>
2020-09-22 16:17:31 +0530

    [swapnaneel]UC6:Reach exact 100 posotion

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,7 +2,7 @@ public class SnakeAndLadder
 {
     public static void main(String args[])
 {
-    int start_pos=0;int number=0;
+    int start_pos=0;int n=0;
     while(start_pos!=100) {
         int die_number = (int) (Math.random() * 6 + 1);
         n++;
